a59e73a53218fba65c1ddd07674b95f03357f8e2
Daniel Millett <millettdanny5@gmail.com>
2022-10-12 23:38:45 +0100

    added module class

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dan
+ */
 public class Module {
+    private String name;
+    private static double id;
+    private double moduleId;
+    private ArrayList<Student> students = new ArrayList<Student>();
+    private ArrayList<CourseProgramme> courses;
+    private Lecturer lecturer;
+
+    public Module(String n, ArrayList<CourseProgramme> courses, Lecturer l){
+        this.name = n;
+        this.courses = courses;
+        this.lecturer = l;
+        this.moduleId = id++;
+    }
+
+    public void addStudents(ArrayList<Student> students){
+        this.students.addAll(students);
+    }
+
+    public ArrayList<Student> getStudents() {
+        return this.students;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<CourseProgramme> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<CourseProgramme> courses) {
+        this.courses = courses;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+
 }
---
+++ src/test/java/CourseProgrammeTest.java
@@ -0,0 +1,4 @@
+import junit.framework.TestCase;
+public class CourseProgrammeTest extends TestCase {
+  
+}
