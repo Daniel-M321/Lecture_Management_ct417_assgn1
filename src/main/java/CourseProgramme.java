7c9c1a1f5c61d23214dcd4d0d0becba3f74b17c1
Daniel Millett <millettdanny5@gmail.com>
2022-10-12 23:21:33 +0100

    included jodatime and created course class

--- src/main/java/CourseProgramme.java
+++ src/main/java/CourseProgramme.java
@@ -1,2 +1,51 @@
+import java.util.ArrayList;
+import org.joda.time.DateTime;
+
 public class CourseProgramme {
+    private String name;
+    private ArrayList<Module> modules = new ArrayList<Module>();
+    private ArrayList<Student> students = new ArrayList<Student>();
+    private DateTime startDate;
+    private DateTime endDate;
+
+    public CourseProgramme(String n){
+        this.name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addStudents(ArrayList<Student> students){
+        this.students.addAll(students);
+    }
+
+    public ArrayList<Student> getStudents() {
+        return this.students;
+    }
+
+    public void addModule(Module module){
+        this.modules.add(module);
+    }
+
+    public ArrayList<Module> getModules() {
+        return this.modules;
+    }
+
+    public void setStartDate(DateTime date){
+        this.startDate = date;
+    }
+
+    public DateTime getStartDate(){
+        return this.startDate;
+    }
+
+    public void setEndDate(DateTime date){
+        this.endDate = date;
+    }
+
+    public DateTime getEndDate(){
+        return this.endDate;
+    }
+
 }
